fdde80e3dd975e5aea9b02f7c6b7a66884ce3285
Adi G <adig@jhu.edu>
2018-04-23 15:08:29 -0400

    debug1

--- R/get_repo_install_order.R
+++ R/get_repo_install_order.R
@@ -24,6 +24,7 @@ get_repo_dep_mat = function(
   dcfs = get_remote_package_dcf(repos)
 
   info = lapply(dcfs, function(tmp) {
+    message(tmp)
     if (is.na(tmp)) {
       L = list(Package = NA,
                Version = NA)
